e95c2574d5ab4ca2cf3aa0e1eee87b8378a7f084
uzzu <himeatball@gmail.com>
2021-05-24 15:46:01 +0900

    Reset patch version

--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Project
 
 private const val majorVersion: Int = 0
 private const val minorVersion: Int = 4
-private val patchVersion: Int = 1
+private val patchVersion: Int = 0
 private const val coreModuleName = "core"
 
 val Project.publishingGroupId: String
